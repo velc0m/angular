c81bee891ff00dd87ccafb546967279c8d8a217b
Litvinenka Vitali <lvitali@setplex.com>
2017-11-10 13:17:15 +0300

    6. ng-filter.

--- src/main.js
+++ src/main.js
@@ -7,51 +7,88 @@ app.filter('reverse', function () {
 });
 
 app.controller("MyController", function ($scope, filmService) {
-   $scope.data = {
-       message: ""
-   };
+    $scope.data = {
+        message: ""
+    };
 
-   $scope.films = filmService.getFilms();
+    $scope.films = filmService.getFilms();
 
-   $scope.reversedMessage = function (message) {
-       return message.split("").reverse().join("");
-   }
+    $scope.reversedMessage = function (message) {
+        return message.split("").reverse().join("");
+    }
 });
 
 //create service
 app.factory("filmService", function () {
-   return {
-     getFilms: function () {
-         return [
-             {
-                 color: "red",
-                 value: "#f00"
-             },
-             {
-                 color: "green",
-                 value: "#0f0"
-             },
-             {
-                 color: "blue",
-                 value: "#00f"
-             },
-             {
-                 color: "cyan",
-                 value: "#0ff"
-             },
-             {
-                 color: "magenta",
-                 value: "#f0f"
-             },
-             {
-                 color: "yellow",
-                 value: "#ff0"
-             },
-             {
-                 color: "black",
-                 value: "#000"
-             }
-         ]
-     }  
-   };
+    return {
+        getFilms: function () {
+            return [
+                {
+                    name: "Peter",
+                    age: 20,
+                    films: {
+                        text: "AngularJS!",
+                        author: "Вова Сидоров",
+                        date: "20/10/2013",
+                        rate: {id: 1, value: "some"}
+                    }
+                },
+                {
+                    name: "Pablo",
+                    age: 55,
+                    films: {
+                        text: "BolerJS!",
+                        author: "Вова Сидоров",
+                        date: "20/10/2013",
+                        rate: {id: 2, value: "some"}
+                    }
+                },
+                {
+                    name: "Linda",
+                    age: 20,
+                    films: {
+                        text: "AngularJS!",
+                        author: "Вова Сидоров",
+                        date: "20/10/2013",
+                        rate: {id: 8, value: "some"}
+                    }
+                },
+                {
+                    name: "Marta",
+                    age: 37,
+                    films: {
+                        text: "AngularJS!",
+                        author: "Вова Сидоров",
+                        date: "20/10/2013",
+                        rate: {id: 8, value: "some"}
+                    }
+                },
+                {
+                    name: "Othello",
+                    age: 20,
+                    films: {
+                        text: "AngularTuTu!",
+                        author: "Вови Сидоров",
+                        date: "20/10/2013",
+                        rate: {id: 1, value: "some"}
+                    }
+                },
+                {
+                    name: "Markus",
+                    age: 32,
+                    films:
+                        {
+                            text: "AngularJS!",
+                            author: "Дуся Сидоров",
+                            date: "20/10/2013",
+                            rate:
+                                {
+                                    id: 5,
+                                    value: "some"
+                                }
+                        }
+                }
+            ]
+        }
+    };
 });
