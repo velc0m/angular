1d514943b0b9ab9c57b106fdcbb5844ddc50c681
Litvinenka Vitali <lvitali@setplex.com>
2017-11-10 12:50:13 +0300

    5. Service.

--- src/main.js
+++ src/main.js
@@ -6,12 +6,52 @@ app.filter('reverse', function () {
     }
 });
 
-app.controller("MyController", function ($scope) {
+app.controller("MyController", function ($scope, filmService) {
    $scope.data = {
        message: ""
    };
 
+   $scope.films = filmService.getFilms();
+
    $scope.reversedMessage = function (message) {
        return message.split("").reverse().join("");
    }
+});
+
+//create service
+app.factory("filmService", function () {
+   return {
+     getFilms: function () {
+         return [
+             {
+                 color: "red",
+                 value: "#f00"
+             },
+             {
+                 color: "green",
+                 value: "#0f0"
+             },
+             {
+                 color: "blue",
+                 value: "#00f"
+             },
+             {
+                 color: "cyan",
+                 value: "#0ff"
+             },
+             {
+                 color: "magenta",
+                 value: "#f0f"
+             },
+             {
+                 color: "yellow",
+                 value: "#ff0"
+             },
+             {
+                 color: "black",
+                 value: "#000"
+             }
+         ]
+     }  
+   };
 });
