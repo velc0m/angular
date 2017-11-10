38fa753616250694d0be3e3b4cbb8f4f74fd1f43
Litvinenka Vitali <lvitali@setplex.com>
2017-11-10 12:02:04 +0300

    3. Method controller.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 var app = angular.module("angularDemo", []);
 app.controller("MyController", function ($scope) {
    $scope.data = {
-       message: "Hello!"
+       message: ""
+   };
+
+   $scope.reversedMessage = function (message) {
+       return message.split("").reverse().join("");
    }
 });
