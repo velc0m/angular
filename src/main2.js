fc36308f3fb772581a3b8471f14e9deff002cdd3
Vitali Litvinenka <velc0m@rambler.ru>
2017-11-12 18:00:03 +0300

    9. Directive types.

--- src/main2.js
+++ src/main2.js
@@ -1,8 +1,49 @@
 var app = angular.module("angularDemo", []);
 
+//element
 app.directive("superman", function () {
-   return {
-       restrict: "E",
-       template: "<div>Hello i'm super man</div>"
-   }
+    return {
+        restrict: "E",
+        template: "<div>Hello i'm super man</div>"
+    }
+});
+
+//attribute
+app.directive("superAttribute", function () {
+    return {
+        restrict: "A",
+        link: function () {
+            alert("attribute working!!!")
+        }
+    }
+});
+
+//attribute
+app.directive("superAttributeNew", function () {
+    return {
+        restrict: "A",
+        link: function () {
+            alert("new attribute working!!!")
+        }
+    }
+});
+
+//class
+app.directive("superClass", function () {
+    return {
+        restrict: "C",
+        link: function () {
+            alert("class working!!!")
+        }
+    }
+});
+
+//comment
+app.directive("superComment", function () {
+    return {
+        restrict: "M",
+        link: function () {
+            alert("comment working!!!")
+        }
+    }
 });
